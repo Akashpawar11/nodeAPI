53f429de3464a195b2355bde9e92ac8d09400b6e
akash pawar <akashpawar1103@gmail.com>
2022-09-29 17:42:13 +0530

    execution done
    Signed-off-by: akash pawar <akashpawar1103@gmail.com>

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
-const { Console } = require('console');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -28,12 +27,14 @@ app.get('/', (req, res) => {
 var database = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'toor',
-    database: 'node-mysql-api'
+    password: '',
+    database: 'nodeapi'
 });
+
 // connect to database
 database.connect();
-// Retrieve all users
+
+// get all users data
 app.get('/users', (req, res) => {
     database.query('SELECT * FROM users', (error, results) => {
         if (error) throw error;
@@ -44,7 +45,7 @@ app.get('/users', (req, res) => {
 
 // Get user information with user ID  
 app.get('/user/:id', (req, res) => {
-    let user_id = req.params.id;
+    let user_id = req.params.id
     if (!user_id) {
         return res.status(400).send({ message: 'Please provide user_id' });
     }
@@ -57,37 +58,51 @@ app.post('/user', (req, res) => {
     let id = req.body.id;
     let name = req.body.name;
     let email = req.body.email;
-    let created_at = req.body.created_at;
-    database.query("INSERT INTO users values (?,?,?,?) ", [id , name , email ,created_at], (error, results) => {
-        if (error) throw error;
+    let contact = req.body.contact;
+    let role = req.body.role;
+    database.query("INSERT INTO users values (?,?,?,?,?) ", [id, name, email, contact,role], (error, results) => {
+        if (error) {
+            throw error;
+        }
         return res.send({ data: results, message: 'New user has been created successfully.' });
     });
 });
+
 //  Update user with id
-// app.put('/user', (req, res) => {
-//     let user_id = req.body.user_id;
-//     let user = req.body.user;
-//     if (!user_id || !user) {
-//         return res.status(400).send({ message: 'Please provide user and user_id' });
-//     }
-//     database.query("UPDATE users SET user = ? WHERE id = ?", [user, user_id], (error, results) => {
-//         if (error) throw error;
-//         return res.send({ data: results, message: 'user has been updated successfully.' });
-//     });
-// });
-// //  Delete user
-// app.delete('/user', (req, res) => {
-//     let user_id = req.body.user_id;
-//     if (!user_id) {
-//         return res.status(400).send({ message: 'Please provide user_id' });
-//     }
-//     database.query('DELETE FROM users WHERE id = ?', [user_id], (error, results, fields) => {
-//         if (error) throw error;
-//         return res.send({ data: results, message: 'User has been updated successfully.' });
-//     });
-// });
-// set port
+app.put('/user', (req, res) => {
+    let id1 = parseInt(req.body.id)
+    console.log(id1)
+    console.log(typeof(id1))
+    let name = (req.body.name).toString();
+    console.log(name)
+    let email = (req.body.email).toString();
+    console.log(email)
+    let contact = (req.body.contact).toString();
+    console.log(contact)
+    let role = (req.body.role).toString();
+    console.log(role)
+    if (!id1) {
+        return res.status(400).send({ message: 'Please provide user and user_id' });
+    }
+    database.query("UPDATE users SET name=?, email=?, contact=? ,role=? WHERE id=?", [name,email,contact,role,id1], (error, results) => {
+        if (error) throw error;
+        return res.send({data: results, message: 'user has been updated successfully.' });
+    });
+});
+
+app.delete('/user/:id', function (req, res) {
+    let id = req.params.id;
+    if (!id) {
+        return res.status(400).send({ error: true, message: 'Please provide user_id' });
+    }
+    database.query('DELETE FROM users WHERE id = ?', [id], function (error, results){
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'User has been removed successfully.' });
+    });
+    }); 
+
 app.listen(3000, () => {
     console.log('node-express-api is running on http://localhost:3000');
 });
+
 module.exports = app;
